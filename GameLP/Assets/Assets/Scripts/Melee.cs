bf904b5794e2e70cfaf0fa52dfd769e41f94b2c5
santiCG <62231604+santiCG@users.noreply.github.com>
2022-10-08 22:32:04 -0500

    Comentarios script combate a melee
    se deshabitilo el sistema de daño que estaba implementado en este script con el fin de implementar uno diferente que permita integrar a los diferentes enemigos y personajes dentro del juego

--- GameLP/Assets/Assets/Scripts/Melee.cs
+++ GameLP/Assets/Assets/Scripts/Melee.cs
@@ -2,55 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Melee : MonoBehaviour
+public class MeleeCombat : MonoBehaviour
 {
-    /*
-    // NOTA:
-    // Este script se debe agregar a Clay
-    // para que el script funcione hay que crear un objeto vacio en el objeto de clay,
-    // luego es necesario arrastar ese objeto vacio slot llamado Controlador Golpe.
-    public class combat : MonoBehaviour
+    [SerializeField] private Transform controladorGolpe;
+
+    [SerializeField] private float radioGolpe;
+
+    [SerializeField] private float danoGolpe;
+
+
+    void Start()
     {
-        [SerializeField] private Transform controladorGolpe;
 
-        [SerializeField] private float radioGolpe;
+    }
 
-        [SerializeField] private float danoGolpe;
-
-
-        void Start()
+    void Update()
+    {
+        // cuando se presione el espacio se ejecutara la funcion de Golpe
+        if (Input.GetButtonDown("Jump"))
         {
-
-        }
-
-        void Update()
-        {
-            if (Input.GetButtonDown("Jump"))
-            {
-                Golpe();
-            }
-        }
-
-        private void Golpe()
-        {
-            Debug.Log("atacando");
-
-            Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radioGolpe);
-
-            foreach (Collider2D colisionador in objetos)
-            {
-                if (colisionador.CompareTag("Enemy"))
-                {
-                    colisionador.transform.GetComponent<EnemyLvl1>().tomarDano(danoGolpe);
-                }
-            }
-        }
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.red;
-            Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+            Golpe();
         }
     }
-    */
+
+    // accion a realizar cuando se oprima el boton de golpear
+    private void Golpe()
+    {
+        Debug.Log("atacando");
+
+        Collider2D[] objetos = Physics2D.OverlapCircleAll(controladorGolpe.position, radioGolpe);
+
+        // Enviar informacion del dano al enemigo
+        /*foreach (Collider2D colisionador in objetos)
+        {
+            if (colisionador.CompareTag("Enemy"))
+            {
+                colisionador.transform.GetComponent<Enemy>().tomarDano(danoGolpe);
+            }
+        }*/
+    }
+
+    // muestra el circulo de accion del golpe de Clay
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(controladorGolpe.position, radioGolpe);
+    }
 }
