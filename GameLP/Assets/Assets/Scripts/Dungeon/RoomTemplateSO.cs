947512b640a71662cd5cb0ef58d63f439f99b1f6
santiCG <santiagocardenasgomez@gmail.com>
2022-10-14 21:29:21 -0500

    Seccion siete

---
+++ GameLP/Assets/Assets/Scripts/Dungeon/RoomTemplateSO.cs
@@ -0,0 +1,49 @@
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Room_", menuName = "Scriptable Object/Dungeon/Room")]
+
+public class RoomTemplateSO : ScriptableObject
+{
+    [HideInInspector] public string guid;
+
+    public GameObject prefab; 
+
+    [Space(10)]
+    [Header("ROOM PREFAB")]
+
+    [HideInInspector] public GameObject previousPrefab;
+
+    [Space(10)]
+    [Header("ROOM CONFIGURATION")]
+
+    public RoomNodeTypeSO roomNodeType;
+
+    public Vector2Int lowerBounds;
+
+    public Vector2Int upperBounds;
+
+    [SerializeField] public List<Doorway> doorwayList;  
+
+    public Vector2Int[] spawnPositionArray;
+
+    public List<Doorway> GetDoorwayList()
+    {
+        return doorwayList;
+    }
+
+    private void OnValidate()
+    {
+        if(guid == "" || previousPrefab != prefab)
+        {
+            guid = GUID.Generate().ToString();
+            previousPrefab = prefab;
+            EditorUtility.SetDirty(this);
+        }
+
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(doorwayList), doorwayList);
+
+        HelperUtilities.ValidateCheckEnumerableValues(this, nameof(spawnPositionArray), spawnPositionArray);
+    }
+}
