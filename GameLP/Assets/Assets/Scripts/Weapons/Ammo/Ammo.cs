7eb8affec8c8404b9e0b6eee1b2bdbb4d3757129
Alejandro <alejolr1218@gmail.com>
2022-10-31 09:53:18 -0500

    Enemy Patterns y Blaster

--- GameLP/Assets/Assets/Scripts/Weapons/Ammo/Ammo.cs
+++ GameLP/Assets/Assets/Scripts/Weapons/Ammo/Ammo.cs
@@ -35,20 +35,23 @@ public class Ammo : MonoBehaviour, IsFireable
             isAmmoMaterialSet = true;
         }
 
-        Vector3 distanceVector = fireDirectionVector * ammoSpeed * Time.deltaTime;
-
-        transform.position += distanceVector;
-
-        ammoRange -= distanceVector.magnitude;
-
-        if (ammoRange < 0f)
+        if (!overrideAmmoMovement)
         {
-            if(ammoDetails.isPlayerAmmo)
-            {
-                StaticEventHandler.CallMultiplierEvent(false);
-            }
+            Vector3 distanceVector = fireDirectionVector * ammoSpeed * Time.deltaTime;
 
-            DisableAmmo();
+            transform.position += distanceVector;
+
+            ammoRange -= distanceVector.magnitude;
+
+            if (ammoRange < 0f)
+            {
+                if (ammoDetails.isPlayerAmmo)
+                {
+                    StaticEventHandler.CallMultiplierEvent(false);
+                }
+
+                DisableAmmo();
+            }
         }
     }
 
