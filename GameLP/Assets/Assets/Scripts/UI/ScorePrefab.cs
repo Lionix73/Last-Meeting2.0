9e1b3c2db4ae655061cf039411449e0781cf0404
santiCG <santiagocardenasgomez@gmail.com>
2022-11-04 18:07:29 -0500

    Fix vida y high scores UI

--- GameLP/Assets/Assets/Scripts/UI/ScorePrefab.cs
+++ GameLP/Assets/Assets/Scripts/UI/ScorePrefab.cs
@@ -8,10 +8,18 @@ public class ScorePrefab : MonoBehaviour
     public TextMeshProUGUI levelTMP;
     public TextMeshProUGUI scoreTMP;
 
-    private void OnValidate() {
+    #region Validation
+#if UNITY_EDITOR
+    // Validate the scriptable object details entered
+    private void OnValidate()
+    {
         HelperUtilities.ValidateCheckNullValue(this, nameof(rankTMP), rankTMP);
         HelperUtilities.ValidateCheckNullValue(this, nameof(nameTMP), nameTMP);
         HelperUtilities.ValidateCheckNullValue(this, nameof(levelTMP), levelTMP);
         HelperUtilities.ValidateCheckNullValue(this, nameof(scoreTMP), scoreTMP);
     }
+
+
+#endif
+    #endregion
 }
